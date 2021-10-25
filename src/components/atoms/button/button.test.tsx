092f0519b9d006fe4a3648c31390f73a54da9b51
Josep Vidal <josepvidalvidal@gmail.com>
2021-10-25 21:46:34 +0200

    feat(jest): configured jest and added test to button

---
+++ src/components/atoms/button/button.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "./button";
+import { render, fireEvent } from "@testing-library/react";
+
+describe("Button test", () => {
+  test("Should render the specified label", () => {
+    const onClick = jest.fn();
+    const label = "This is a button";
+
+    const { getByText, getByRole } = render(
+      <Button onClick={onClick}>{label}</Button>
+    );
+
+    const button = getByRole("button");
+
+    expect(getByText(label)).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
