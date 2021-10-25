0105c511d6a6fd936f92dedad320873f051047f7
Josep Vidal <josepvidalvidal@gmail.com>
2021-10-25 21:20:23 +0200

    feat: home page features improved

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,21 @@
 import Logos from "components/atoms/logos";
 import Card from "components/molecules/card";
 import {
+  BeakerIcon,
   BookmarkAltIcon,
+  CakeIcon,
+  ChevronDownIcon,
   CubeTransparentIcon,
+  FilmIcon,
+  LocationMarkerIcon,
+  LockClosedIcon,
+  MenuIcon,
   PencilIcon,
   PhotographIcon,
-  PuzzleIcon,
 } from "@heroicons/react/outline";
 import { QuestionMarkCircleIcon } from "@heroicons/react/solid";
+import Button from "components/atoms/button";
+import CopyButton from "components/organisms/copy-button";
 
 import styles from "./app.module.css";
 
@@ -15,43 +23,79 @@ const features = [
   {
     name: "Vite",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Faster and leaner development experience for modern web projects.",
     logo: CubeTransparentIcon,
     docs: "https://vitejs.dev/",
   },
   {
     name: "React",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "JavaScript library for building user interfaces.",
     logo: PencilIcon,
     docs: "https://reactjs.org/",
   },
   {
     name: "TypeScript",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Strongly typed programming language that builds on JavaScript.",
     logo: BookmarkAltIcon,
     docs: "https://www.typescriptlang.org/",
   },
   {
-    name: "Tailwind",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    name: "Tailwind with JIT",
+    description: "A utility-first CSS framework packed with classes.",
     logo: PhotographIcon,
     docs: "https://tailwindcss.com/",
   },
   {
-    name: "PostCSS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    logo: PuzzleIcon,
-    docs: "https://postcss.org/",
+    name: "Jest",
+    description: "Testing Framework with a focus on simplicity.",
+    logo: QuestionMarkCircleIcon,
+    docs: "https://jestjs.io/",
   },
   {
-    name: "Jest",
+    name: "CSS Modules",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    logo: QuestionMarkCircleIcon,
+      "CSS file in which all class names and animation names are scoped locally by default.",
+    logo: LockClosedIcon,
+    docs: "https://github.com/css-modules/css-modules",
+  },
+  {
+    name: "ESLint",
+    description: "Find and fix problems in your JavaScript code.",
+    logo: BeakerIcon,
+    docs: "https://eslint.org/",
+  },
+  {
+    name: "Prettier",
+    description: "An opinionated code formatter.",
+    logo: MenuIcon,
+    docs: "https://jestjs.io/#",
+  },
+  {
+    name: "Husky",
+    description:
+      "Lint your commit messages, run tests, lint code, etc... when you commit or push.",
+    logo: CakeIcon,
+    docs: "https://jestjs.io/#",
+  },
+  {
+    name: "Commit-lint",
+    description: "Helps your team adhering to a commit convention.",
+    logo: FilmIcon,
+    docs: "https://jestjs.io/#",
+  },
+  {
+    name: "Atomic design",
+    description:
+      "We’re not designing pages, we’re designing systems of components.",
+    logo: LocationMarkerIcon,
+    docs: "https://jestjs.io/#",
+  },
+  {
+    name: "Absolute imports",
+    description:
+      "Import resource using its full path from the project’s src folder",
+    logo: ChevronDownIcon,
     docs: "https://jestjs.io/#",
   },
 ];
@@ -59,36 +103,56 @@ const features = [
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
-      <Logos.Vite className={styles.viteLogo} />
-      <div className={styles.header}>
-        <h1 className={styles.headerTitle}>
-          React + TypeScript + Tailwind @ Vite template
-        </h1>
-      </div>
-      <div className={styles.content}>
-        <section className={styles.features}>
-          {features.map((props, index) => (
-            <div
-              key={index}
-              className={styles.cardWrapper}
-              style={{ animationDelay: `${index * 0.1 + 0.2}s` }}
-            >
-              <Card
-                title={props.name}
-                description={props.description}
-                Icon={props.logo}
-                cta={
-                  <div>
-                    <a className={styles.cta} href={props.docs} target="_blank">
-                      Visit documentation →
-                    </a>
-                  </div>
-                }
-              />
-            </div>
-          ))}
-        </section>
-      </div>
+      <header className={styles.header}>
+        <h3 className={styles.headerTopTitle}>Vite Template</h3>
+        <h1 className={styles.headerTitle}>React + TypeScript + Tailwind</h1>
+        <p className={styles.headerDescription}>
+          Bootstrap your web projects faster than ever. Comes with:{" "}
+          <code className={styles.headerDescriptionCode}>CSS-Modules</code>,{" "}
+          <code className={styles.headerDescriptionCode}>Jest</code>,{" "}
+          <code className={styles.headerDescriptionCode}>Husky</code>,{" "}
+          <code className={styles.headerDescriptionCode}>Commit-lint</code>,{" "}
+          <code className={styles.headerDescriptionCode}>ESLint</code>,{" "}
+          <code className={styles.headerDescriptionCode}>Prettier</code> and{" "}
+          <code className={styles.headerDescriptionCode}>
+            Atomic organization for components
+          </code>{" "}
+          configured and ready to go.
+        </p>
+        <div className={styles.viteLogoContainer}>
+          <Logos.Vite className={styles.viteLogo} />
+        </div>
+      </header>
+      <section className={styles.copy}>
+        <div className={styles.copyInner}>
+          <a href="https://github.com/jvidalv/vite-react-typescript-tailwind-template">
+            <Button>Visit on Github</Button>
+          </a>
+          <CopyButton text="npx degit jvidalv/vite-react-typescript-tailwind-template my-app" />
+        </div>
+      </section>
+      <section className={styles.features}>
+        {features.map((props, index) => (
+          <div
+            key={index}
+            className={styles.cardWrapper}
+            style={{ animationDelay: `${index * 0.1 + 0.1}s` }}
+          >
+            <Card
+              title={props.name}
+              description={props.description}
+              Icon={props.logo}
+              cta={
+                <div className={styles.ctaContainer}>
+                  <a className={styles.cta} href={props.docs} target="_blank">
+                    Visit documentation →
+                  </a>
+                </div>
+              }
+            />
+          </div>
+        ))}
+      </section>
     </main>
   );
 };
